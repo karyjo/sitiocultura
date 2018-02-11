a37ce21e4a1cf412ae5614ca3afbc617acfe91fc
karyjo <kjulieska93@hotmaiil.com>
2018-02-11 16:30:21 -0600

    añadiendo cambios

--- index.php
+++ index.php
@@ -1,4 +1,45 @@
-<?php
-/**
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>mipagina</title>
+</head>
+<body>
+    <header style="background-color: black; width: 100%; height: 200px;">
+        <section style="background-color: orange; width: 100%; height: 50px;">
+        <p>soy un parrafo</p>
+        </section>
+        <nav style="background-color: blue; width: 100%; height: 100px;">
+            <a href="#">ir a jupiter</a>
+        </nav>
 
-*/
+
+    </header>
+
+    <section>
+        <h1>hola soy la seccion 1</h1>
+    </section>
+
+<section>
+    <h1>hola soy la seccion 2</h1>
+</section>
+
+<section>
+
+    <h1>hola soy la seccion 3</h1>
+</section>
+
+<section>
+    <h1> hola soy la seccion 4</h1>
+</section>
+
+<section>
+    <h1>hola soy la seccion 5</h1>
+</section>
+
+
+
+</html>
